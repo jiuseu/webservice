948ea7dc426e85dc09eea5ad447bb693b99f668d
lee hyun woo <kaway0418@naver.com>
2022-10-26 15:36:12 +0900

    10월 26일 수정

---
+++ src/test/java/com/jojoidu/book/webservice/WebserviceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jojoidu.book.webservice;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

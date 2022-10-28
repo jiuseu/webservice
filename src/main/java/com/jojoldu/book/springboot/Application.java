b5a5086e3b3228e3a29464a819db03789618f512
lee hyun woo <kaway0418@naver.com>
2022-10-28 10:47:35 +0900

    10월 28일 10시 16분 수정

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -2,7 +2,7 @@ package com.jojoldu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 //@EnableJpaAuditing //JPA Auditing 활성화
 @SpringBootApplication

d75fd21da76afb4a80a283ad2d29a4d31f7901ce
lee hyun woo <kaway0418@naver.com>
2022-10-31 22:34:06 +0900

    10월 31일 16시 32분 수정

---
+++ scripts/deploy.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# 자주 사용하는 값 변수에 저장
+REPOSITORY=/home/ec2-user/app/step1
+PROJECT_NAME=webservice
+
+# build의 결과물 (jar 파일) 특정 위치로 복사
+echo "> build 파일 복사"
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(pgrep -fl webservice | grep jar | awk '{print $1}')
+
+echo "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
+if [ -z "$CURRENT_PID" ]; then
+        echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+        echo "> kill -15 $CURRENT_PID"
+        kill -15 $CURRENT_PID
+        sleep 5
+fi
+
+echo "> 새 애플리케이션 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | grep -v "plain" | tail -n 1)
+
+echo "> JAR_Name: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
+nohup java -jar \
+        -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+        -Dspring.profiles.active=real \
+        $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+
+
